9484db4b1d0483b5dc54b3e7fc9db6e992066257
stevekim <stevekim@stevekims-MacBook-Air.local>
2019-06-10 19:12:41 -0400

    Displays the Instructions

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const DOMAIN = "https://www.thecocktaildb.com/api.php";
 const button = document.querySelector("button");
-const list = document.querySelector(".drink-list");
+const drinkList = document.querySelector(".drink-list");
+let currentRecipe;
+
+//const displayIngrediants = 
+
+const displayInstruction = (drinkCard, drink) => {
+  drinkCard.addEventListener('click', async () => {
+    const resp = await axios(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${drink.idDrink}`)
+    const drinkInstructions = document.createElement('p');
+    drinkInstructions.innerText = resp.data.drinks[0].strInstructions;
+    drinkCard.appendChild(drinkInstructions);
+  })
+}
 
 const renderList = (drinks) => {
   drinks.forEach((drink) => {
@@ -11,10 +23,10 @@ const renderList = (drinks) => {
     drinkName.innerText = drink.strDrink;
     drinkCard.appendChild(drinkName);
     drinkCard.appendChild(drinkImage);
-    list.appendChild(drinkCard);
+    drinkList.appendChild(drinkCard);
+    displayInstruction(drinkCard, drink);
   })
 }
-
 
 const searchDrink = async (name) => {
   const resp = await axios(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${name}`);
@@ -23,7 +35,7 @@ const searchDrink = async (name) => {
 
 button.addEventListener('click', (ev) => {
   ev.preventDefault();
-  list.innerHTML = " ";
+  drinkList.innerHTML = " ";
   let inputDrink = document.querySelector('input');
   searchDrink(inputDrink.value);
 });
